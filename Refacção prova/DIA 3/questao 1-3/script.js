a4af2b7d00a3487ddbbaffe9770efb8e5399243a
Henrique Pappa <105671025+he-nz@users.noreply.github.com>
2026-04-23 23:32:15 -0300

    Update script.js

--- Refacção prova/DIA 3/questao 1-3/script.js
+++ Refacção prova/DIA 3/questao 1-3/script.js
@@ -1,27 +1,29 @@
-function revelar() {
-    const foto = document.getElementById('avatar');
-    foto.src = './img/_vinicius_junior.png';
+let revelado = false;
 
-    const nome = document.getElementById('Nome');
-    const rank = document.getElementById('Rank');
-    const dataNas = document.getElementById('Data_Nas');
-    const altura = document.getElementById('Alutra');
-    const posicao = document.getElementById('Posição ');
+function add() {
+    if (revelado) return; 
 
-    nome.firstChild.textContent = 'Vinícius José P. de Oliveira Jr. ';
-    rank.textContent = '9,5';
-    dataNas.textContent = 'Nasc: 12/07/2000 (25 anos)';
-    altura.textContent = 'Altura: 1,76 m';
-    posicao.textContent = 'Ponta-esquerda / Atacante';
+    const foto = document.getElementById('FotoJogador');
+    foto.src = 'img/_vinicius_junior.jpg';
 
-    const placeholders = document.querySelectorAll('.placeholder');
-    placeholders.forEach(p => {
-        p.classList.remove('placeholder', 'col-8', 'col-10', 'col-6', 'col-9');
-        p.classList.add('card-text'); 
-    });
+    const elNome = document.getElementById('Nome');
+    const elRank = document.getElementById('Rank');
+    const elData = document.getElementById('Data_Nas');
+    const elAltura = document.getElementById('Alutra');
+    const elPosicao = document.getElementById('Posição ');
+    const btn = document.getElementById('btnAcao');
+    elNome.innerHTML = `Vinícius Júnior <span id="Rank" class="badge badge-sucesso">9,5</span>`;
+    
+    elData.classList.remove('placeholder');
+    elData.innerHTML = `<strong>Nascimento:</strong> 12/07/2000 (25 anos)`;
 
-    const glows = document.querySelectorAll('.placeholder-glow');
-    glows.forEach(g => g.classList.remove('placeholder-glow'));
+    elAltura.classList.remove('placeholder');
+    elAltura.innerHTML = `<strong>Altura:</strong> 1,76 m`;
 
-    rank.classList.replace('text-bg-secondary', 'text-bg-success');
+    elPosicao.classList.remove('placeholder');
+    elPosicao.innerHTML = `<strong>Posição:</strong> Ponta-esquerda / Atacante`;
+
+    btn.textContent = "Jogador Revelado";
+    btn.disabled = true;
+    revelado = true;
 }
